073fd306dc368948e1379e616eacc43933cfbabe
Leekogram <33817077+Leekogram@users.noreply.github.com>
2020-09-21 06:21:07 +0100

    Add project files.

---
+++ XamarinWeatherApp/XamarinWeatherApp/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinWeatherApp
+{
+    class Constants
+    {
+        public const string OpenWeatherMapEndpoint = "https://api.openweathermap.org/data/2.5/weather";
+        public const string OpenWeatherMapAPIKey = "69d073305c3e27d12644268eb1be011c";
+    }
+}
---
+++ XamarinWeatherApp/XamarinWeatherApp/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinWeatherApp
+{
+   
+    public partial class MainPage : ContentPage
+    {
+        RestService _restService;
+        public MainPage()
+        {
+            InitializeComponent();
+            _restService = new RestService();
+        }
+        async void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(cityEntry.Text))
+            {
+                OnDisplayAlertButtonClicked();
+                WeatherData weatherData = await _restService.GetWeatherDataAsync(GenerateRequestUri(Constants.OpenWeatherMapEndpoint));
+                BindingContext = weatherData;
+            }
+           
+
+        }
+        string GenerateRequestUri(string endpoint)
+        {
+            string requestUri = endpoint;
+            requestUri += $"?q={cityEntry.Text}";
+            requestUri += "&units=imperial"; // or units=metric
+            requestUri += $"&APPID={Constants.OpenWeatherMapAPIKey}";
+            return requestUri;
+        }
+        async void OnDisplayAlertButtonClicked()
+        {
+            await DisplayAlert("Alert", "Fetching data...", "OK");
+        }
+    }
+}
